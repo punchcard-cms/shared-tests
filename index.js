8b7eaea7116185eb03cc64a229a498e0577a2068
Scott Nath <snath@us.ibm.com>
2016-12-09 11:57:47 -0500

    :new: plugins build utility added

--- index.js
+++ index.js
@@ -2,8 +2,10 @@
 
 const plugins = require('./lib/input-plugins');
 const fixtures = require('./tests/fixtures');
+const utils = require('./lib/utils');
 
 module.exports = {
   fixtures,
   plugins,
+  utils,
 };
